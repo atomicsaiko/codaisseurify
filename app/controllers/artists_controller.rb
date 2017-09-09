fb424657e4a6e669c7499bf31fb094ba184ba104
atomicsaiko <atomicsaiko@gmail.com>
2017-09-09 19:51:40 +0200

    Visitor can delete Artist. It also delete associated model objects.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,5 +5,16 @@ class ArtistsController < ApplicationController
 
   def show
     @artist = Artist.find(params[:id])
+    @song = @artist.songs
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+
+    if @artist.destroy
+      redirect_to artists_path
+    else
+      redirect_to artist_path
+    end
   end
 end
